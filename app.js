309c42bb65152f387186d33c8ac4728786f4f729
İpek Koçhisarlı <ipekkochisarli@gmail.com>
2022-09-07 22:17:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const apiKey = "api_key=381bcd8ce3b8e3204a448c8640f81155";
 const imgURL = "https://image.tmdb.org/t/p/w500";
 const main = document.querySelector("#main");
-const searchBar = document.querySelector('#search')
+const searchBar = document.querySelector("#search");
 const movies = {
   apiKey: "api_key=381bcd8ce3b8e3204a448c8640f81155",
-  fetchMovies:function () {
+  fetchMovies: function () {
     fetch(
       "https://api.themoviedb.org/3/discover/movie?sort_by-popularity.desc&" +
         apiKey
     )
       .then((response) => response.json())
-      .then((data) => {console.log(data.results);
-      showMovies(data.results);
-  });
+      .then((data) => {
+        console.log(data.results);
+        showMovies(data.results);
+      });
   },
 };
-
 
 function showMovies(data) {
   main.innerHTML = "";
 
   data.forEach((movies) => {
-    const { title, poster_path, vote_average, overview} = movies;
+    const { title, poster_path, vote_average, overview } = movies;
     const movieElement = document.createElement("div");
     movieElement.classList.add("movie");
-    movieElement.innerHTML= `
+    movieElement.innerHTML = `
             <img
             src="${imgURL + poster_path}"
             alt="${title}"
@@ -40,7 +40,7 @@ function showMovies(data) {
             </div>
         
         `;
-     main.appendChild(movieElement);
+    main.appendChild(movieElement);
   });
 }
 movies.fetchMovies();
